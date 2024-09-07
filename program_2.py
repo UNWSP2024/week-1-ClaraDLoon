8365a4dd47e28abccd176ac95f5342260086ccdb
ClaraDLoon <ClaraDLoon@outlook.com>
2024-09-06 20:34:45 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,6 +1,11 @@
+#09/06/2024
+#Clara Riley
+#Modified Hello World Program B
+
 def proverbs_22_29():
-    # Write your Proverbs 22:29 code below
-    print('...')
-    
+    print('"Do you see a man skillful of his work?')
+    print('He will stand before kinds;')
+    print('he will not stand before obscure men."')
+    print('(Proverbs 22:29)')
 
 proverbs_22_29()
